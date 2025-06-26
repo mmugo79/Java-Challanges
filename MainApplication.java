bc5e07c9191a9f8cbe2f5f89315038c5dc3f1721
Moses <159237123+mmugo79@users.noreply.github.com>
2025-06-26 17:18:13 +0300

    MainApplication.java(Abstract main class)

--- MainApplication.java
+++ MainApplication.java
@@ -1,4 +1,4 @@
-public class MainApplication {
+public class MainApplication{
     public static void main(String[] args) {
         Admin curAdmin = new Admin(1234, "Stephan", "xa1@LW42%3");
         User curUser = new User(4455, "John");
