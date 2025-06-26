42976f03a692282c349f8d9db6c7df8e5a3bef8e
Moses <159237123+mmugo79@users.noreply.github.com>
2025-06-26 16:33:00 +0300

    Admin.java(inheritance)-initial commit

---
+++ Admin.java
@@ -0,0 +1,9 @@
+class Admin extends Account {
+    private String AuthPassword;
+
+    public Admin(int _id, String _name, String _password) {
+        id = _id;
+        name = _name;
+        AuthPassword = _password;
+    }
+}
