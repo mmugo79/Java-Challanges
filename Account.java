f1a3b6a93a7596dc2bf1ed2c993de4c787828152
Moses <159237123+mmugo79@users.noreply.github.com>
2025-06-26 16:31:54 +0300

    Account.java(inheritance)-initial commit

---
+++ Account.java
@@ -0,0 +1,10 @@
+abstract class Account {
+    protected int id;
+    protected String name;
+    private DataSource myData;
+
+    public void performOperation(DataSource _myData) {
+        myData = _myData;
+        myData.execute();
+    }
+}
