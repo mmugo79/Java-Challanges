aef49f9c4daa92c916b54442a80688b822bd4142
Moses <159237123+mmugo79@users.noreply.github.com>
2025-07-03 16:00:46 +0300

    Exception handling

---
+++ importjava.java
@@ -0,0 +1,11 @@
+import java.io;
+class ArithmeticJava{
+    public static void main(string[]args)
+
+{
+int number1 = 100;
+int number2 = 0;
+int answer = answer1 / answer2;
+system.out.println("Answer: " + ans);
+}
+}
